73eec921032c4b7509f0ccc1153e15f5ab94bfb3
Tyler Newton <55857663+TylerNewton@users.noreply.github.com>
2020-05-27 09:40:07 -0700

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 var express = require('express');
+var data  = require('./data/test.json');
+
+/*
+imort data from './data/test.json';
+
+*/
+
 var app = express();
 const PORT = 3000;
 
@@ -9,4 +16,5 @@ app.use(express.static(__dirname));
 
 app.listen(PORT,()=>{
   console.log(`App is running on port ${PORT}`);
+  console.log(data);
 });
